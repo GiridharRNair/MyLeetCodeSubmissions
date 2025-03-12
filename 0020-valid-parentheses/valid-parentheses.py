a9da1cf62569684fb6e503e1adb7cc2a8317ec90
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-03-12 13:18:36 -0500

    Time: 0 ms (100.00%) Space: 17.8 MB (80.92%)

--- 0020-valid-parentheses/valid-parentheses.py
+++ 0020-valid-parentheses/valid-parentheses.py
@@ -8,8 +8,11 @@ class Solution:
 
         stack = []
         for p in s:
-            if stack and p in p_map and p_map[p] == stack[-1]:
-                stack.pop()
+            if stack and p in p_map:
+                if p_map[p] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
             else:
                 stack.append(p)
         #     print(stack)
