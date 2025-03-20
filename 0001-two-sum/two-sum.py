4eb7a15605ed77d221255cd0415d19e81dd89b2b
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-03-20 13:34:14 -0500

    Time: 3 ms (51.15%) Space: 18.7 MB (65.47%)

--- 0001-two-sum/two-sum.py
+++ 0001-two-sum/two-sum.py
@@ -1,13 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        m = {}
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] + nums[j] == target and i != j:
-                    return [i, j]
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for idx, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], idx]
+            seen[num] = idx
