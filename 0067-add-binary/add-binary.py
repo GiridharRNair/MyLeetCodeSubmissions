797a17ca47e8ae6805876455054b82a9c3706f43
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-03-25 14:02:27 -0500

    Time: 0 ms (100.00%) Space: 17.9 MB (52.37%)

---
+++ 0067-add-binary/add-binary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        out = ""
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += int(a[i])
+                i -= 1
+            if j >= 0:
+                carry += int(b[j])
+                j -= 1
+            out += str(carry % 2)
+            carry //= 2
+        return out[::-1]
