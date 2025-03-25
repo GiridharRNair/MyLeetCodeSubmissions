81198da67974880ce9b23d3909e17cb80ef3e3d4
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-03-25 15:09:13 -0500

    Time: 12 ms (40.93%) Space: 31.6 MB (53.95%)

--- 0217-contains-duplicate/contains-duplicate.py
+++ 0217-contains-duplicate/contains-duplicate.py
@@ -2,8 +2,4 @@ from collections import Counter
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        n_count = Counter(nums)
-        for n in n_count:
-            if n_count[n] > 1:
-                return True
-        return False
+        return len(nums) != len(set(nums))
