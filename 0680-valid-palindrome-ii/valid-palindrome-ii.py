5a4c6fc771e5b02baaa2e7872ed6e70470506b2a
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-03-27 22:33:19 -0500

    Time: 30 ms (92.01%) Space: 18 MB (82.25%)

--- 0680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def is_palindrome(s):
-            left = 0
-            right = len(s) - 1
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
+        # def is_palindrome(s):
+        #     left = 0
+        #     right = len(s) - 1
+        #     while left < right:
+        #         if s[left] != s[right]:
+        #             return False
+        #         left += 1
+        #         right -= 1
+        #     return True
 
         left = 0
         right = len(s) - 1
@@ -17,7 +17,7 @@ class Solution:
             if s[left] != s[right]:
                 string_1 = s[:left] + s[left + 1:]
                 string_2 = s[:right] + s[right + 1:]
-                return is_palindrome(string_1) or is_palindrome(string_2)
+                return string_1 == string_1[::-1] or string_2 == string_2[::-1]
             left += 1
             right -= 1
         return True
