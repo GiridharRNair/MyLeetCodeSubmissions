7fdaf4e31964ab44c08f074c36d2ab9e0d53618d
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-03-28 16:13:18 -0500

    Time: 119 ms (12.22%) Space: 23.4 MB (12.17%)

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,14 +1,15 @@
-from queue import PriorityQueue
+import heapq
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        min_heap = PriorityQueue()
+        # min_heap = PriorityQueue()
+        h = []
         for point in points:
             d = math.sqrt(math.pow(point[0], 2) + math.pow(point[1], 2))
-            min_heap.put((d, [point[0], point[1]]))
+            heapq.heappush(h, (d, [point[0], point[1]]))
         
         out = []
         for i in range(k):
-            d, arr = min_heap.get()
+            d, arr = heappop(h)
             out.append(arr)
         return out
