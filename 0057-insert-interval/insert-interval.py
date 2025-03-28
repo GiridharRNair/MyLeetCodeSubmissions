9900ee02150dea41dd507c9891ce78e7249e16f2
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-03-27 23:34:52 -0500

    Time: 2 ms (59.67%) Space: 19.8 MB (40.89%)

--- 0057-insert-interval/insert-interval.py
+++ 0057-insert-interval/insert-interval.py
@@ -1,21 +1,32 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        out = []
-        idx = 0
+        # out = []
+        # idx = 0
         
-        while idx < len(intervals) and intervals[idx][1] < newInterval[0]:
-            out.append(intervals[idx])
-            idx += 1
+        # while idx < len(intervals) and intervals[idx][1] < newInterval[0]:
+        #     out.append(intervals[idx])
+        #     idx += 1
 
-        while idx < len(intervals) and intervals[idx][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[idx][0])
-            newInterval[1] = max(newInterval[1], intervals[idx][1])
-            idx += 1
-        out.append(newInterval)
+        # while idx < len(intervals) and intervals[idx][0] <= newInterval[1]:
+        #     newInterval[0] = min(newInterval[0], intervals[idx][0])
+        #     newInterval[1] = max(newInterval[1], intervals[idx][1])
+        #     idx += 1
+        # out.append(newInterval)
             
-        while idx < len(intervals):
-            out.append(intervals[idx])
-            idx += 1
+        # while idx < len(intervals):
+        #     out.append(intervals[idx])
+        #     idx += 1
     
+        # return out
+        out = []
+        for idx, interval in enumerate(intervals):
+            if interval[0] > newInterval[1]:
+                out.append(newInterval)
+                return out + intervals[idx:]
+            elif interval[1] < newInterval[0]:
+                out.append(interval)
+            else:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+        out.append(newInterval)
         return out
-         
