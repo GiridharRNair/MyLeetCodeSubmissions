0c1ef314cb730e26e0460d77efdd0341268ca794
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-04-03 21:01:17 -0500

    Time: 0 ms (100.00%) Space: 18.1 MB (71.52%)

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def post_order(node):
+            if not node:
+                return 0, None
+
+            left = post_order(node.left)
+            right = post_order(node.right)
+
+            if left[0] > right[0]:
+                return left[0] + 1, left[1]
+            elif left[0] < right[0]:
+                return right[0] + 1, right[1]
+
+            return left[0] + 1, node
+
+        return post_order(root)[1]
