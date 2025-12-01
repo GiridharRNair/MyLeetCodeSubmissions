332fc78ba07cf2f69334f03e02462186ab2c7383
Giridhar Nair <49298134+GiridharRNair@users.noreply.github.com>
2025-12-01 17:26:52 -0600

    Time: 47 ms (95.61%) Space: 31.1 MB (20.98%)

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
@@ -2,15 +2,18 @@ class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
         batteries.sort()
 
-        extra = sum(batteries[:-n])
-
         computer_batteries = batteries[-n:]
+        extra = sum(batteries[:-n])
+        # print(extra // 0)
+        # [7, 5, 10, 12]
+        # [5, 10, 12]
+        # [10, 10, 12]
 
         for i in range(n - 1):
-            if computer_batteries[i + 1] - computer_batteries[i] <= extra // (i + 1):
-                extra -= (computer_batteries[i + 1] - computer_batteries[i]) * (i + 1)
+            battery_difference = computer_batteries[i + 1] - computer_batteries[i]
+            if battery_difference <= (extra // (i + 1)):
+                extra -= battery_difference * (i + 1)
             else:
-                return computer_batteries[i] + extra // (i + 1)
+                return computer_batteries[i] + (extra // (i + 1))
 
-        return computer_batteries[-1] + extra // n
-
+        return computer_batteries[-1] + (extra // n)
