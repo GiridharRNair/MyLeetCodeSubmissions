3f97751e7e18bd048fd4ecee55def132eeb23874
Giridhar Nair <49298134+GiridharRNair@users.noreply.github.com>
2025-12-02 23:06:01 -0600

    Time: 171 ms (5.14%) Space: 63.4 MB (91.05%)

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        y_cord_store = defaultdict(int)
+        res = 0
+        total_sum = 0
+        MOD = 10 ** 9 + 7
+
+        for x, y in points:
+            y_cord_store[y] += 1
+
+        for y_count in y_cord_store.values():
+            unique_trapazoids = (y_count * (y_count - 1)) // 2
+            print((y_count * (y_count - 1)) // 3, unique_trapazoids)
+            res += (unique_trapazoids * total_sum)
+            total_sum += unique_trapazoids
+        return res % MOD
