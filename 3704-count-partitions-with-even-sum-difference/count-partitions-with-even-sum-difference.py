cf85841caf0db336e37aad5e53558e2a895545dc
Giridhar Nair <49298134+GiridharRNair@users.noreply.github.com>
2025-12-04 18:52:03 -0600

    Time: 0 ms (100.00%) Space: 17.6 MB (92.20%)

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
@@ -1,20 +1,5 @@
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:
-        n = len(nums)
-        prefix_sums = [0] * n
-
-        prefix_sums[0] = nums[0]
-        for i in range(1, n):
-            prefix_sums[i] = prefix_sums[i - 1] + nums[i]
-
-        total_sum = prefix_sums[-1]
-        # print(prefix_sums)
-        res = 0
-        for i in range(n - 1):
-            prefix_sum = prefix_sums[i]
-            
-            if (prefix_sum - (total_sum - prefix_sum)) % 2 == 0:
-                # print(total_sum, prefix_sum)
-                res += 1
-        
-        return res
+        if sum(nums) % 2 == 0:
+            return len(nums) - 1
+        return 0
